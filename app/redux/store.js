7e28aa51cc014cbb50ec6106a611e4da762402ae
Ridwan Balogun <balogunridwan@gmail.com>
2020-03-28 11:42:11 +0100

    Refactored

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,5 +1,13 @@
- import {createStore, applyMiddleware, compose} from 'redux';
- import thunk from 'redux-thunk';
- import rootReducer from './rootReducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
 
- const enhancer = compose(applyMiddleware(thunk));
+const enhancer = compose(applyMiddleware(thunk));
+
+const store = createStore(
+    rootReducer,
+    enhancer)
+
+export default store;
+
+
