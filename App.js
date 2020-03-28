5ed9cab426e618c4ea07fa0e39e14b1834c0a413
Ridwan Balogun <balogunridwan@gmail.com>
2020-03-28 10:19:55 +0100

    Refactoring

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is React Native News App!</Text>
-    </View>
-  );
+import Routes from './app/config/routes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Routes />
+    );
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
